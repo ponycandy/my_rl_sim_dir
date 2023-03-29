00baf0f303c23388694736a6e89c874492487d58
pony <ponycandy666@126.com>
2023-03-29 13:40:29 +0800

    补充

--- PPO_demo.py
+++ PPO_demo.py
@@ -11,6 +11,7 @@ import numpy as np
 
 import gym
 # import roboschool
+from tensorboardX import SummaryWriter
 
 from PPO import PPO
 
@@ -27,13 +28,7 @@ def train():
     max_training_timesteps = int(3e6)   # break training loop if timeteps > max_training_timesteps
 
     print_freq = max_ep_len * 10        # print avg reward in the interval (in num timesteps)
-    log_freq = max_ep_len * 2           # log avg reward in the interval (in num timesteps)
-    save_model_freq = int(1e5)          # save model frequency (in num timesteps)
 
-    action_std = 0.6                    # starting std for action distribution (Multivariate Normal)
-    action_std_decay_rate = 0.05        # linearly decay action_std (action_std = action_std - action_std_decay_rate)
-    min_action_std = 0.1                # minimum action_std (stop decay after action_std <= min_action_std)
-    action_std_decay_freq = int(2.5e5)  # action_std decay frequency (in num timesteps)
     #####################################################
 
     ## Note : print/log frequencies should be > than max_ep_len
@@ -51,106 +46,20 @@ def train():
     random_seed = 0         # set random seed if required (0 = no random seed)
     #####################################################
 
-    print("training environment name : " + env_name)
 
     env = gym.make(env_name)
 
-    # state space dimension
-    state_dim = env.observation_space.shape[0]
-
-    # action space dimension
-    if has_continuous_action_space:
-        action_dim = env.action_space.shape[0]
-    else:
-        action_dim = env.action_space.n
-
-    ###################### logging ######################
-
-    #### log files for multiple runs are NOT overwritten
-    log_dir = "PPO_logs"
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    log_dir = log_dir + '/' + env_name + '/'
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    #### get number of log files in log directory
-    run_num = 0
-    current_num_files = next(os.walk(log_dir))[2]
-    run_num = len(current_num_files)
-
-    #### create new log file for each run
-    log_f_name = log_dir + '/PPO_' + env_name + "_log_" + str(run_num) + ".csv"
-
-    print("current logging run number for " + env_name + " : ", run_num)
-    print("logging at : " + log_f_name)
-    #####################################################
-
-    ################### checkpointing ###################
-    run_num_pretrained = 0      #### change this to prevent overwriting weights in same env_name folder
-
-    directory = "PPO_preTrained"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    directory = directory + '/' + env_name + '/'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
 
 
-    checkpoint_path = directory + "PPO_{}_{}_{}.pth".format(env_name, random_seed, run_num_pretrained)
-    print("save checkpoint path : " + checkpoint_path)
-    #####################################################
 
 
-    ############# print all hyperparameters #############
-    print("--------------------------------------------------------------------------------------------")
-    print("max training timesteps : ", max_training_timesteps)
-    print("max timesteps per episode : ", max_ep_len)
-    print("model saving frequency : " + str(save_model_freq) + " timesteps")
-    print("log frequency : " + str(log_freq) + " timesteps")
-    print("printing average reward over episodes in last : " + str(print_freq) + " timesteps")
-    print("--------------------------------------------------------------------------------------------")
-    print("state space dimension : ", state_dim)
-    print("action space dimension : ", action_dim)
-    print("--------------------------------------------------------------------------------------------")
-    if has_continuous_action_space:
-        print("Initializing a continuous action space policy")
-        print("--------------------------------------------------------------------------------------------")
-        print("starting std of action distribution : ", action_std)
-        print("decay rate of std of action distribution : ", action_std_decay_rate)
-        print("minimum std of action distribution : ", min_action_std)
-        print("decay frequency of std of action distribution : " + str(action_std_decay_freq) + " timesteps")
-    else:
-        print("Initializing a discrete action space policy")
-    print("--------------------------------------------------------------------------------------------")
-    print("PPO update frequency : " + str(update_timestep) + " timesteps")
-    print("PPO K epochs : ", K_epochs)
-    print("PPO epsilon clip : ", eps_clip)
-    print("discount factor (gamma) : ", gamma)
-    print("--------------------------------------------------------------------------------------------")
-    print("optimizer learning rate actor : ", lr_actor)
-    print("optimizer learning rate critic : ", lr_critic)
-    if random_seed:
-        print("--------------------------------------------------------------------------------------------")
-        print("setting random seed to ", random_seed)
-        torch.manual_seed(random_seed)
-        env.seed(random_seed)
-        np.random.seed(random_seed)
-    #####################################################
 
-    print("============================================================================================")
-
-    ################# training procedure ################
-
-    # initialize a PPO agent
     replaybuff=PPO_Buffer()
     optimizer = PPO()
     optimizer.set_Replaybuff(replaybuff,gamma,lr_actor,lr_critic)
 
     actor_proxy=PPO_Actor_Proxy()
-    actorNet=Actor(4,24,2)
+    actorNet=Actor_Softmax(4,24,2)
     criticM=Critic_PPO(4,24,1)
     actor_proxy.setNet(actorNet,criticM)
     actor_proxy.set_action_dim(2)
@@ -158,15 +67,7 @@ def train():
     actor_proxy.set_range([1,1],[0,0])
     optimizer.setNet(actorNet,criticM,actor_proxy)
 
-    # track total training time
-    start_time = datetime.now().replace(microsecond=0)
-    print("Started training at (GMT) : ", start_time)
 
-    print("============================================================================================")
-
-    # logging file
-    log_f = open(log_f_name,"w+")
-    log_f.write('episode,timestep,reward\n')
 
     # printing and logging variables
     print_running_reward = 0
@@ -177,86 +78,37 @@ def train():
 
     time_step = 0
     i_episode = 0
-
+    state = env.reset()
+    lastobs=TensorTypecheck(state[0])
+    current_ep_reward = 0
+    epoch=1
+    TIMESTAMP = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
+    writer =SummaryWriter("./my_log_dir/"+TIMESTAMP)
     # training loop
-    while time_step <= max_training_timesteps:
-
-        state = env.reset()
-        lastobs=TensorTypecheck(state[0])
-        current_ep_reward = 0
-
-        for t in range(1, max_ep_len+1):
-
+    while True:
             # select action with policy
-            action=actor_proxy.response(lastobs)
-            obs, reward, done, _ ,_= env.step(action)
+        action=actor_proxy.response(lastobs)
+        obs, reward, done, truncated ,_= env.step(action)
+        replaybuff.appendnew(lastobs,actor_proxy.action,obs,reward)
+        lastobs=TensorTypecheck(obs)
+        if done or truncated:
+            replaybuff.ResetNotify()#only notify once!!
+            state = env.reset()
+            lastobs=TensorTypecheck(state[0])
+            writer.add_scalar("reward",current_ep_reward,epoch)
+            epoch+=1
+            current_ep_reward = 0
 
-            # saving reward and is_terminals
-            # replaybuff.appendnew()
-            replaybuff.appendnew(lastobs,actor_proxy.action,obs,reward)
-            lastobs=TensorTypecheck(obs)
-            if done:
-                replaybuff.ResetNotify()#only notify once!!
+#这里使用logger记录会比较好
+            #因为提升是迅速的
+        time_step +=1
+        current_ep_reward += reward
+        optimizer.update()
 
-            time_step +=1
-            current_ep_reward += reward
-            optimizer.update()
 
-            # log in logging file
-            if time_step % log_freq == 0:
 
-                # log average reward till last episode
-                log_avg_reward = log_running_reward / log_running_episodes
-                log_avg_reward = round(log_avg_reward, 4)
 
-                log_f.write('{},{},{}\n'.format(i_episode, time_step, log_avg_reward))
-                log_f.flush()
 
-                log_running_reward = 0
-                log_running_episodes = 0
-
-            # printing average reward
-            if time_step % print_freq == 0:
-
-                # print average reward till last episode
-                print_avg_reward = print_running_reward / print_running_episodes
-                print_avg_reward = round(print_avg_reward, 2)
-
-                print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, time_step, print_avg_reward))
-
-                print_running_reward = 0
-                print_running_episodes = 0
-
-            # save model weights
-            if time_step % save_model_freq == 0:
-                print("--------------------------------------------------------------------------------------------")
-                print("saving model at : " + checkpoint_path)
-                print("model saved")
-                print("Elapsed Time  : ", datetime.now().replace(microsecond=0) - start_time)
-                print("--------------------------------------------------------------------------------------------")
-
-            # break; if the episode is over
-            if done:
-                break
-
-        print_running_reward += current_ep_reward
-        print_running_episodes += 1
-
-        log_running_reward += current_ep_reward
-        log_running_episodes += 1
-
-        i_episode += 1
-
-    log_f.close()
-    env.close()
-
-    # print total training time
-    print("============================================================================================")
-    end_time = datetime.now().replace(microsecond=0)
-    print("Started training at (GMT) : ", start_time)
-    print("Finished training at (GMT) : ", end_time)
-    print("Total training time  : ", end_time - start_time)
-    print("============================================================================================")
 
 
 if __name__ == '__main__':
--- PTorchEnv/a_demo_PPO_Cartpole.py
+++ PTorchEnv/a_demo_PPO_Cartpole.py
@@ -1,93 +1,80 @@
-from actor_proxy import actor_proxy
-import gymnasium as gym
-from PTorchEnv.CartpoleTCP import CartpoleTCP
-from PTorchEnv.PPO_ReplayMemory import PPO_ReplayMemory
-from PTorchEnv.Prioritized_Replaybuffer import Prioritized_Replaybuffer
-from PTorchEnv.ProbOpt import ProbOpt
-import random
-from PyTorchTool.RLDebugger import RLDebugger
-from PTorchEnv.matrix_copt_tool import deepcopyMat
-from tensorboardX import SummaryWriter
-from PTorchEnv.RL_parameter_calc import RL_Calculator
+import os
+import glob
+import time
 from datetime import datetime
+from PTorchEnv.PPO_Buffer import PPO_Buffer
+from PTorchEnv.PPO_Actor_Proxy import PPO_Actor_Proxy
+from PTorchEnv.model import Actor_Softmax,Critic_PPO,Actor
+from PTorchEnv.Typechecker import TensorTypecheck
 import torch
-from model import Critic
-from model import Actor
-from model import Actor_Softmax
-from model import Critic_PPO
-from PPO_Actor_Proxy import PPO_Actor_Proxy
+import numpy as np
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-RL_logger=RL_Calculator()
-TIMESTAMP = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
-writer =SummaryWriter("./my_log_dir/"+TIMESTAMP)
-# ploterr=RLDebugger()
-optimizer=ProbOpt()
-BATCHSIZE=10000
-replaybuff=PPO_ReplayMemory(BATCHSIZE)
+import gym
+# import roboschool
+from tensorboardX import SummaryWriter
 
-optimizer.set_Replaybuff(replaybuff,128,0.99,1e-4,1e-3,4800)
-# envnow=CartpoleTCP(8001,"127.0.0.1")
-envnow = gym.make("CartPole-v1")
+from PPO import PPO
 
+
+####### initialize environment hyperparameters ######
+env_name = "CartPole-v1"
+env = gym.make(env_name)
+
+
+
+
+
+
+replaybuff=PPO_Buffer()
+optimizer = PPO()
+optimizer.set_Replaybuff(replaybuff,0.99, 0.0003, 0.001)
 
 actor_proxy=PPO_Actor_Proxy()
-actorNet=Actor_Softmax(4,24,2).to(device)
-
-
-actor_proxy.setNet(actorNet)
-actor_proxy.set_range([2],[0])
+actorNet=Actor_Softmax(4,24,2)
+criticM=Critic_PPO(4,24,1)
+actor_proxy.setNet(actorNet,criticM)
+actor_proxy.set_action_dim(2)
 actor_proxy.setActFlag("Discrete")
-actor_proxy.use_eps_flag=1
-actor_proxy.set_action_dim(1)
-
-Criticm=Critic_PPO(4,24,1).to(device)
-
-# critic只用来判断value值，不用来判断Q值，所以无需动作的输入
-# 这就是Q值和V值区别的最直接例子
-optimizer.set_NET(actorNet,Criticm,actor_proxy)
+actor_proxy.set_range([1,1],[0,0])
+optimizer.setNet(actorNet,criticM,actor_proxy)
 
 
-state, info = envnow.reset()
-lastobs = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-step_done=0
-total_reward=0
+
+
+time_step = 0
+
+state = env.reset()
+lastobs=TensorTypecheck(state[0])
+current_ep_reward = 0
 epoch=1
+TIMESTAMP = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
+writer =SummaryWriter("./my_log_dir/"+TIMESTAMP)
+# training loop
 while True:
+    # select action with policy
     action=actor_proxy.response(lastobs)
-
-    observation, reward, terminated, truncated, _ = envnow.step(action)
-    total_reward+=reward
-    reward = torch.tensor([reward], device=device)
-    done = terminated or truncated
-    if terminated:
-        obs=None
-    else:
-        obs = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-
+    obs, reward, done, truncated ,_= env.step(action)
     replaybuff.appendnew(lastobs,actor_proxy.action,obs,reward)
-
-    if terminated or truncated:
-        # PPO不可避免的需要进行terminated检测，所以需要补上下面这个非常规行:总是在进行重置的时候呼出下面这个repllay的函数
-        replaybuff.ResetNotify()
-        state, info = envnow.reset()
-        lastobs = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        writer.add_scalar("reward",total_reward,epoch)
+    lastobs=TensorTypecheck(obs)
+    if done or truncated:
+        replaybuff.ResetNotify()#only notify once!!
+        state = env.reset()
+        lastobs=TensorTypecheck(state[0])
+        writer.add_scalar("reward",current_ep_reward,epoch)
         epoch+=1
-        total_reward=0
-    else:
-        lastobs=deepcopyMat(obs)
+        current_ep_reward = 0
 
-    loss=optimizer.loss_calc()  #将critic和actor的update分开，这里应该就没有什么soft update了吧
-
-
-
-
-    step_done+=1
+    #这里使用logger记录会比较好
+    #因为提升是迅速的
+    time_step +=1
+    current_ep_reward += reward
+    optimizer.update()
 
 
-# 各个优化器和模块的更改不导致上面主程序的更改
+
+
+
+
 
 
 
